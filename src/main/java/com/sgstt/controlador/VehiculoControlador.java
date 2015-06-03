044afc42365ffb2ab871f4c36e99edec2a45adf1
Dr4nzer <Dr4nzer@Dr4nzer>
2015-06-02 20:44:56 -0500

    Mejoras en CSS y Validaciones

--- src/main/java/com/sgstt/controlador/VehiculoControlador.java
+++ src/main/java/com/sgstt/controlador/VehiculoControlador.java
@@ -12,10 +12,6 @@ import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
 import javax.faces.context.FacesContext;
 
-/**
- *
- * @author SAM
- */
 @ManagedBean(name = "vehiculoControlador")
 @ViewScoped
 public class VehiculoControlador {
@@ -59,11 +55,29 @@ public class VehiculoControlador {
     
     private boolean esVistaValida(){
         boolean resultado = true;
-        if(!esTipoVehiculoValido()){
+        if(!esDescripcionValida()){
+            Utilitario.enviarMensajeGlobalError("Debe ingresar una descripcion");
+            resultado = false;
+        }else if(!esPlacaValida()){
+            Utilitario.enviarMensajeGlobalError("Debe ingresar una placa");
+            resultado = false;
+        }else if(!esTipoVehiculoValido()){
             Utilitario.enviarMensajeGlobalError("Debe seleccionar un Tipo de Vehiculo");
             resultado = false;
         }else if(!esMarcaValida()){
             Utilitario.enviarMensajeGlobalError("Debe seleccionar una Marca");
+            resultado = false;
+        }else if(!esAFabricacionValido()){
+            Utilitario.enviarMensajeGlobalError("Debe ingresar un año de fabricacion");
+            resultado = false;
+        }else if(!esCapMaximaValida()){
+            Utilitario.enviarMensajeGlobalError("Debe ingresar una capacidad Maxima");
+            resultado = false;
+        }else if(!esCapRecomendadaValida()){
+            Utilitario.enviarMensajeGlobalError("Debe ingresar una capacidad recomendada");
+            resultado = false;
+        }else if(!esColorValido()){
+            Utilitario.enviarMensajeGlobalError("Debe seleccionar un color");
             resultado = false;
         }
         return resultado;
@@ -76,7 +90,33 @@ public class VehiculoControlador {
     private boolean esMarcaValida(){
         return vehiculo.getMarca().getId().intValue() != 0;
     }
-
+    
+    private boolean esColorValido(){
+        return vehiculo.getColor() != "";
+    }
+    private boolean esDescripcionValida(){
+        return vehiculo.getDescripcion() != "";
+    }
+    
+    private boolean esPlacaValida(){
+        return vehiculo.getPlaca() != "";
+    }            
+    
+    private boolean esCapMaximaValida(){
+        return vehiculo.getCapacidad_max() != "";
+    }
+    
+    private boolean esCapRecomendadaValida(){
+        return vehiculo.getCapacidad_recomendada() != "";
+    }
+    
+    private boolean esAFabricacionValido(){
+        String año = vehiculo.getAño_fabricacion();
+        boolean aux = true;
+        if(año.equals("")) { aux = false; }
+        else if(Short.parseShort(año) > 2015) { aux = false; }
+        return aux;
+    }
     /* GETTERS AND SETTERS */
     
     public HibernatePaginador<Vehiculo> getVehiculoPaginador() {
